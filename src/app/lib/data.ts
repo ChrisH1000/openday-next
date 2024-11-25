766821b6ff14ed59600e05ab2b4dd743f51cfe34
Chris Hill <christopher.hill@ecu.edu.au>
2024-11-25 13:02:19 +0800

    test: remove create client to test deploy error

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,15 +1,16 @@
-import { createClient } from '@vercel/postgres';
+// import { createClient } from '@vercel/postgres';
 
 export async function connectToDB() {
-  const client = createClient();
-  await client.connect();
+  return true;
+  // const client = createClient();
+  // await client.connect();
 
-  try {
-    if (client) {
-      console.log('Connected to database');
-      return client;
-    }
-  } catch (error) {
-    console.error('Error connecting to database:', error);
-  }
+  // try {
+  //   if (client) {
+  //     console.log('Connected to database');
+  //     return client;
+  //   }
+  // } catch (error) {
+  //   console.error('Error connecting to database:', error);
+  // }
 }
