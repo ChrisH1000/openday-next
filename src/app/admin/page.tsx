2edb23073cc8663daffcf7bde76f5afa9f68358b
Chris Hill <christopher.hill@ecu.edu.au>
2024-11-25 11:06:50 +0800

    feat: created test script to connect to DB

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,10 @@
-export default function Page() {
+import { connectToDB } from "@/app/lib/data";
+
+export default async function Page() {
+  const client = await connectToDB();
   return (
     <>
+      {client && <p>Connected to DB</p>}
       <h1>Admin</h1>
       <p>Massa urna magnis dignissim id euismod porttitor vitae etiam viverra nunc at adipiscing sit morbi aliquet mauris porttitor nisi, senectus pharetra, ac porttitor orci.</p>
     </>)
