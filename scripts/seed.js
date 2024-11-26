311062caa349e8ee5c203a7b5096f56e464426b0
Chris Hill <christopher.hill@ecu.edu.au>
2024-11-26 09:06:33 +0800

    feat: created seed data and sql for events and sessions

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,12 +1,12 @@
 const { db } = require("@vercel/postgres");
-const { opendays } = require("../src/app/lib/placeholder-data.js");
+const { opendays, events, sessions } = require("../src/app/lib/placeholder-data.js");
 
 async function seedOpendays(client) {
   try {
     await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
     // Create the "users" table if it doesn't exist
     const createTable = await client.sql`
-      CREATE TABLE IF NOT EXISTS opendays (
+      CREATE TABLE IF NOT EXISTS openday (
         id UUID DEFAULT uuid_generate_v1mc() PRIMARY KEY,
         title VARCHAR(255) NOT NULL UNIQUE,
         campus VARCHAR(255) NOT NULL,
@@ -16,13 +16,13 @@ async function seedOpendays(client) {
       );
     `;
 
-    console.log(`Created "opendays" table`);
+    console.log(`Created "openday" table`);
 
     // Insert data into the "users" table
     const insertedOpendays = await Promise.all(
       opendays.map(async (openday) => {
         return client.sql`
-        INSERT INTO opendays (id, title, campus, starttime, endtime, status)
+        INSERT INTO openday (id, title, campus, starttime, endtime, status)
         VALUES (${openday.id}, ${openday.title}, ${openday.campus}, ${openday.starttime}, ${openday.endtime}, ${openday.status})
         ON CONFLICT (id) DO NOTHING;
       `;
@@ -32,7 +32,7 @@ async function seedOpendays(client) {
 
     return {
       createTable,
-      opendays: insertedOpendays,
+      openday: insertedOpendays,
     };
   } catch (error) {
     console.error("Error seeding opendays:", error);
@@ -40,9 +40,86 @@ async function seedOpendays(client) {
   }
 }
 
+async function seedEvents(client) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+    // Create the "users" table if it doesn't exist
+    const createTable = await client.sql`
+      CREATE TABLE IF NOT EXISTS event (
+        id UUID DEFAULT uuid_generate_v1mc() PRIMARY KEY,
+        title VARCHAR(255) NOT NULL UNIQUE,
+        description VARCHAR(255) NOT NULL,
+        interests VARCHAR(255) NOT NULL,
+        openday_fk UUID REFERENCES openday(id)
+      );
+    `;
+
+    console.log(`Created "event" table`);
+
+    // Insert data into the "users" table
+    const insertedEvents = await Promise.all(
+      events.map(async (event) => {
+        return client.sql`
+        INSERT INTO event (id, title, description, interests, openday_fk)
+        VALUES (${event.id}, ${event.title}, ${event.description}, ${event.interests}, ${event.openday_fk})
+        ON CONFLICT (id) DO NOTHING;
+      `;
+      })
+    );
+    console.log(`Seeded ${insertedEvents.length} events`);
+
+    return {
+      createTable,
+      event: insertedEvents,
+    };
+  } catch (error) {
+    console.error("Error seeding events:", error);
+    throw error;
+  }
+}
+
+async function seedSessions(client) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+    // Create the "users" table if it doesn't exist
+    const createTable = await client.sql`
+      CREATE TABLE IF NOT EXISTS session (
+        id UUID DEFAULT uuid_generate_v1mc() PRIMARY KEY,
+        starttime INT NOT NULL,
+        endtime INT NOT NULL,
+        event_fk UUID REFERENCES event(id)
+      );
+    `;
+
+    console.log(`Created "session" table`);
+
+    // Insert data into the "users" table
+    const insertedSessions = await Promise.all(
+      sessions.map(async (session) => {
+        return client.sql`
+        INSERT INTO session (id, starttime, endtime, event_fk)
+        VALUES (${session.id}, ${session.starttime}, ${session.endtime}, ${session.event_fk})
+        ON CONFLICT (id) DO NOTHING;
+      `;
+      })
+    );
+    console.log(`Seeded ${insertedSessions.length} sessions`);
+
+    return {
+      createTable,
+      session: insertedSessions,
+    };
+  } catch (error) {
+    console.error("Error seeding sessions:", error);
+    throw error;
+  }
+}
+
 async function main() {
   const client = await db.connect();
   await seedOpendays(client);
+  await seedEvents(client);
+  await seedSessions(client);
   await client.end();
 }
 
--- src/app/lib/placeholder-data.js
+++ src/app/lib/placeholder-data.js
@@ -25,6 +25,59 @@ const opendays = [
   }
 ];
 
+const events = [
+  {
+    id: "87bdf8ab-f660-490b-bd85-bb0121c2de7a",
+    title: "School of Business and Law drop-in session",
+    description: "Come and chat to us about courses and careers in Business or Law.",
+    interests: "Business & Law,Business,Law",
+    openday_fk: "a5e08164-b12b-4e72-9191-2f7daadb4e88"
+  },
+  {
+    id: "7e7f7c0a-d02b-4021-ad76-c70891c49138",
+    title: "Health Science Course Information",
+    description: "Explore how majors in Health Promotion, Nutrition, Occupational & Environmental Health & Safety, and Occupational Safety & Health lead to diverse career opportunities.",
+    interests: "Medical & Health Sciences,Health Science,Nutrition & Dietetics,Occupational Health & Safety,Public Health",
+    openday_fk: "a5e08164-b12b-4e72-9191-2f7daadb4e88"
+  },
+  {
+    id: "7f62c50c-b2b9-40c4-aaca-f55db39f02ed",
+    title: "Hand hygiene with the Glitter Bug",
+    description: "How clean are your hands? Check them out with the special UV Glitter Bug!",
+    interests: "Nursing & Midwifery",
+    openday_fk: "a5e08164-b12b-4e72-9191-2f7daadb4e88"
+  }
+];
+
+const sessions = [
+  {
+    id: "ef63d799-8001-4eb3-930a-c8a462617189",
+    starttime: 1679796000,
+    endtime: 1679797800,
+    event_fk: "87bdf8ab-f660-490b-bd85-bb0121c2de7a"
+  },
+  {
+    id: "4888d744-c66a-475d-93ca-f9999732dbf0",
+    starttime: 1679796000,
+    endtime: 1679797800,
+    event_fk: "7e7f7c0a-d02b-4021-ad76-c70891c49138"
+  },
+  {
+    id: "1406a2e7-4c7c-4c56-80db-c4d7908ffaf7",
+    starttime: 1679796000,
+    endtime: 1679797800,
+    event_fk: "7f62c50c-b2b9-40c4-aaca-f55db39f02ed"
+  },
+  {
+    id: "aa821652-0283-4bef-8503-7ce5413a0232",
+    starttime: 1679797800,
+    endtime: 1679799600,
+    event_fk: "7f62c50c-b2b9-40c4-aaca-f55db39f02ed"
+  }
+];
+
 module.exports = {
   opendays,
+  events,
+  sessions
 };
