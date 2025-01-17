fde7b57074eafc12cc93a8c32c8ac63f0d041955
Chris Hill <christopher.hill@ecu.edu.au>
2025-01-17 10:17:19 +0800

    feat: added auth config and middleware

---
+++ auth.config.ts
@@ -0,0 +1,30 @@
+import type { NextAuthConfig } from 'next-auth';
+
+export const authConfig = {
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+
+      // const isOnAdmin = nextUrl.pathname.startsWith('/admin');
+      // if (isOnAdmin) {
+      //   if (isLoggedIn && auth?.user && !!auth.user.admin) return true;
+      //   return false; // Redirect unauthenticated users to login page
+      // } else if (isLoggedIn) {
+      //   return Response.redirect(new URL('/admin', nextUrl));
+      // }
+
+      const isOnPlanner = nextUrl.pathname.startsWith('/planner');
+      if (isOnPlanner) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/planner', nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [], // Add providers with an empty array for now
+} satisfies NextAuthConfig;
