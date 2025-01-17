8bad6afc7121ee71b127ae2e6f4742f32fd758d8
Chris Hill <christopher.hill@ecu.edu.au>
2025-01-17 15:21:17 +0800

    feat: add logout

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,13 +1,28 @@
-import { getUser } from "@/app/lib/auth";
+import { signOut } from '@/auth';
 
 export default async function Page() {
-  const user = await getUser('user@openday.com', '123456');
-  console.log(user);
+  // const user = await getUser('user@openday.com', '123456');
+  // console.log(user);
   return (
     <>
-      <p>Connected to DB</p>
-      <h1>Admin ${user?.name}</h1>
+      <h1>Admin</h1>
       <p>Massa urna magnis dignissim id euismod porttitor vitae etiam viverra nunc at adipiscing sit morbi aliquet mauris porttitor nisi, senectus pharetra, ac porttitor orci.</p>
+
+      <div className="flex h-full flex-col px-3 py-4 md:px-2">
+        <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+          <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+          <form
+            action={async () => {
+              'use server';
+              await signOut();
+            }}
+          >
+            <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+              <div className="hidden md:block">Sign Out</div>
+            </button>
+          </form>
+        </div>
+      </div>
     </>)
 
 }
