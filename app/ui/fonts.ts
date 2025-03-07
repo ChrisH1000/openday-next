85ec7965359d9dc74b0fb1c47eb16e3bfb4fdc2a
Chris Hill <christopher.hill@ecu.edu.au>
2025-03-07 10:51:23 +0800

    feat: added cabin font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,9 +1,11 @@
-import { Inter, Lusitana } from 'next/font/google';
+import { Inter, Lusitana, Cabin } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 const lusitana = Lusitana({ subsets: ['latin'], weight: '400' });
+const cabin = Cabin({ subsets: ['latin'], weight: '400' });
 
 export {
   inter,
-  lusitana
+  lusitana,
+  cabin,
 }
