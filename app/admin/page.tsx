901ed209daa385399b660c9d69fea9a4915ce4e8
Chris Hill <christopher.hill@ecu.edu.au>
2025-03-07 10:51:47 +0800

    feat: setup opendays ui template

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,19 +1,22 @@
-import { fetchOpendays } from '../lib/data';
+import Opendays from '@/app/ui/admin/opendays';
+import { cabin } from '@/app/ui/fonts';
+import clsx from 'clsx';
+// import Image from 'next/image';
 
 export default async function Page() {
-  const opendays = await fetchOpendays();
-  console.log(opendays);
   return (
-    <>
-      <h1>Admin</h1>
-      <p>Massa urna magnis dignissim id euismod porttitor vitae etiam viverra nunc at adipiscing sit morbi aliquet mauris porttitor nisi, senectus pharetra, ac porttitor orci.</p>
-
+    <main>
+      <h1 className={`${cabin.className} mb-4 text-xl md:text-2xl`}>
+        Admin
+      </h1>
       <div className="flex h-full flex-col px-3 py-4 md:px-2">
         <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
           <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-
+          <div className="flex w-full flex-col md:col-span-4">
+            <Opendays />
+          </div>
         </div>
       </div>
-    </>)
-
+    </main>
+  )
 }
---
+++ app/ui/admin/opendays.tsx
@@ -0,0 +1,74 @@
+import { ArrowPathIcon, PencilSquareIcon } from '@heroicons/react/24/outline';
+import { fetchOpendays } from '../../lib/data';
+import { cabin } from '@/app/ui/fonts';
+import clsx from 'clsx';
+// import { LatestInvoice } from '@/app/lib/definitions';
+export default async function Opendays() { // Make component async, remove the props
+  const opendays = await fetchOpendays();
+  const liveOpendays = opendays.filter((openday) => openday.status === 'live');
+  const underConstructionOpendays = opendays.filter((openday) => openday.status === 'under construction');
+  const archivedOpendays = opendays.filter((openday) => openday.status === 'archived');
+  console.log(opendays);
+  return (
+    <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
+      <div className="bg-green-50 p-2">
+        <h2 className={`${cabin.className} mb-4 text-xl md:text-1xl pt-2`}>Live OpenDays</h2>
+        {liveOpendays.map((openday, i) => {
+          return (
+            <div
+              key={openday.id}
+              className={clsx(
+                'flex flex-row items-center justify-between p-4 bg-white',
+                {
+                  'border-t': i !== 0,
+                },
+              )}
+            >
+              <div className="flex">
+                <div className="min-w-0">
+                  <p className="truncate text-sm md:text-base">
+                    {openday.title}
+                  </p>
+                  <p className="hidden text-sm text-gray-500 sm:block">
+                    {openday.campus}
+                  </p>
+                </div>
+              </div>
+              <div className="flex">
+                <p
+                  className={`${cabin.className} truncate text-sm font-medium md:text-base`}
+                >
+                  {formatTimestamp(openday.starttime)} {formatTimestamp(openday.starttime, false)} till {formatTimestamp(openday.endtime, false)}
+                </p>
+              </div>
+              <div className="flex items-center">
+                <PencilSquareIcon className="h-5 w-5 text-gray-500" />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="flex items-center pb-2 pt-6">
+        <ArrowPathIcon className="h-5 w-5 text-gray-500" />
+        <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
+      </div>
+    </div>
+  );
+
+  function formatTimestamp(timestamp: number, doDate: boolean = true) {
+    const date = new Date(timestamp * 1000); // Convert to milliseconds
+
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Month is 0-based
+    const year = date.getFullYear();
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+
+    if (doDate) {
+      return `${day}/${month}/${year}`;
+    } else {
+      return `${hours}:${minutes}`;
+    }
+  }
+}
