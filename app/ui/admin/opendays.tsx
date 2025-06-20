0dfaa94b5133af45051e01826a161291c59c4f72
ChrisH1000 <christopher.hill@ecu.edu.au>
2025-06-20 14:13:46 +0800

    feat: put loader on create button

--- app/ui/admin/opendays.tsx
+++ app/ui/admin/opendays.tsx
@@ -1,13 +1,22 @@
+"use client";
 import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import OpendayList from './OpendayList';
 import Link from 'next/link';
+import { useLoading } from '@/app/ui/LoadingContext';
+import { useState } from 'react';
 
 export default function Opendays() {
+  const { setLoading } = useLoading();
+
+  const handleCreateClick = () => {
+    setLoading(true);
+  };
+
   return (
     <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
       <div className="flex justify-end mb-4">
-        <Link href="/admin/opendays/create">
-          <button className="bg-blue-600 text-white px-4 py-2 rounded shadow hover:bg-blue-700 transition-colors">
+        <Link href="/admin/opendays/create" prefetch={false} onClick={handleCreateClick}>
+          <button className="bg-blue-600 text-white px-4 py-2 rounded shadow hover:bg-blue-700 transition-colors min-w-[160px]">
             Create new Openday
           </button>
         </Link>
